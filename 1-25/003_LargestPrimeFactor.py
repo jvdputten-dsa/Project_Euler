8484e983134e36aa8a95636081e3fe342f998c6f
papydur <joostvdp92@gmail.com>
2021-07-10 07:21:09 +0200

    test commit from laptop, added Eratosthenes_sieve function

---
+++ 1-25/003_LargestPrimeFactor.py
@@ -0,0 +1,8 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+def Eratosthenes_sieve(number):
+    current_prime = 2
+    [False]*number
+
+
